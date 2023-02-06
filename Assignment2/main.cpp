ea61331cc24ce535d1474bcc83f29ffbee32391e
SydianAndrewChen <SydianAndrewChen@github.com>
2023-02-06 21:09:20 +0800

    add proj

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -27,64 +27,53 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle, Eigen::Vector3f T, Eigen:
 {
 
     //Step 1: Build the Translation Matrix M_trans:
-
     Eigen::Matrix4f M_trans = Eigen::Matrix4f::Identity();
     M_trans << 1, 0, 0, T[0], 0, 1, 0, T[1], 0, 0, 1, T[2], 0, 0, 0, 1; 
 
     //Step 2: Build the Scale Matrix S_trans:
-
     Eigen::Matrix4f S_trans = Eigen::Matrix4f::Identity();
     S_trans(0, 0) *= S(0);
     S_trans(1, 1) *= S(1);
     S_trans(2, 2) *= S(2);
 
-    //Step 3: Implement Rodrigues' Rotation Formular, rotation by angle theta around axix u, then get the model matrix
+    //Step 3: Implement Rodrigues' Rotation Formular, rotation by angle theta around axis u, then get the model matrix
 	// The axis u is determined by two points, u = P1-P0: Eigen::Vector3f P0 ,Eigen::Vector3f P1  
     // Create the model matrix for rotating the triangle around a given axis. // Hint: normalize axis first
     Eigen::Matrix4f R_trans = Eigen::Matrix4f::Identity();
     Eigen::Vector3f axis = (P1 - P0).normalized();
     float radian = rotation_angle * MY_PI / 180.0;
 
+    R_trans <<     
+    cos(radian)+(1-cos(radian))*axis(0)*axis(0),            -sin(radian)*axis(2)+(1-cos(radian))*axis(0)*axis(1),   sin(radian)*axis(1)+(1-cos(radian))*axis(0)*axis(2),    0,
+    sin(radian)*axis(2)+(1-cos(radian))*axis(0)*axis(1),    cos(radian)+(1-cos(radian))*axis(1)*axis(1),            -sin(radian)*axis(0)+(1-cos(radian))*axis(1)*axis(2),   0,
+    -sin(radian)*axis(1)+(1-cos(radian))*axis(0)*axis(2),   sin(radian)*axis(0)+(1-cos(radian))*axis(1)*axis(2),    cos(radian)+(1-cos(radian))*axis(2)*axis(2),            0,
+    0, 0, 0, 1;
+
 	//Step 4: Use Eigen's "AngleAxisf" to verify your Rotation
 	Eigen::AngleAxisf rotation_vector(radian, Vector3f(axis[0], axis[1], axis[2]));  
 	Eigen::Matrix3f rotation_matrix;
 	Eigen::Matrix3f rotation_m = rotation_vector.toRotationMatrix();
 
-    for (size_t i = 0; i < 3; i++)
-    {
-        for (size_t j = 0; j < 3; j++)
-        {
-            R_trans(i, j) = rotation_m(i, j);
-        }
-        
-    }
-    
+    // std::cout << R_trans << std::endl;
+    // std::cout << rotation_m << std::endl;
+
     Eigen::Matrix4f model = M_trans * R_trans * S_trans;
 
 	return model;
 }
 
-
-
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
                                       float zNear, float zFar)
 {
-    // Implement this function
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+    float radian = eye_fov * MY_PI / 180;
+    projection(0, 0) *= (1.0 / (aspect_ratio * tan(radian / 2)));
+    projection(1, 1) *= (1.0 / tan(radian / 2));
+    projection(2, 2) *= -(zFar + zNear) / (zFar - zNear);
+    projection(2, 3) *= -2 * zNear * zFar /(zFar - zNear);
+    projection(3, 2) = -1;
+    projection(3, 3) = 0;
 
-
-    // TODO: Implement this function
-    // Create the projection matrix for the given parameters.
-    // Then return it.
-
-    // frustum -> cubic
-
-    // orthographic projection
-
-    // squash all transformations
-
-    // std::clog << "projection" << std::endl << projection << std::endl; //check
-    Eigen::Matrix4f projection;
-    
     return projection;
 }
 
@@ -107,7 +96,7 @@ int main(int argc, const char** argv)
     rst::rasterizer r(1024, 1024);
 
     // define your eye position "eye_pos" to a proper position
-    Eigen::Vector3f eye_pos(0,0,0);
+    Eigen::Vector3f eye_pos(0,0,-2);
 
     // define a triangle named by "pos" and "ind"
     std::vector<Eigen::Vector3f> pos {Eigen::Vector3f(0.5, 0.0, 1.0), Eigen::Vector3f(-0.5, 0.0, 1.0), Eigen::Vector3f(0.5, 0.5, 1.0)};
@@ -120,13 +109,13 @@ int main(int argc, const char** argv)
     int frame_count = 0;
 
     // added parameters for get_projection_matrix(float eye_fov, float aspect_ratio,float zNear, float zFar)
-    float eye_fov = 45;
+    float eye_fov = 90;
     float aspect_ratio = 1.0;
     float zNear = 0.01;
     float zFar = 100.0;
 
     // Eigen::Vector3f axis(0, 0, 1);
-    Eigen::Vector3f axis(1, 0, 0);
+    Eigen::Vector3f axis(0, 1, 0);
     
     Eigen::Vector3f T(0, 0, 0);
     Eigen::Vector3f S(1, 1, 1);
