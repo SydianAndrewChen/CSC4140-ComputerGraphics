ffa020a6799d2ae290a4373f735fde7ec845c5a6
SydianAndrewChen <SydianAndrewChen@github.com>
2023-03-05 15:56:18 +0800

    finish assignment3

--- Assignment3&4/src/transforms.cpp
+++ Assignment3&4/src/transforms.cpp
@@ -13,24 +13,29 @@ Vector2D operator*(const Matrix3x3 &m, const Vector2D &v) {
 
 Matrix3x3 translate(float dx, float dy) {
 	// Part 3: Fill this in.
-
-
-	return Matrix3x3();
+	auto t = Matrix3x3::identity();
+	t[2][0] = dx;
+	t[2][1] = dy;
+	return t;
 }
 
 Matrix3x3 scale(float sx, float sy) {
 	// Part 3: Fill this in.
-
-
-	return Matrix3x3();
+	auto s = Matrix3x3::identity();
+	s[0][0] = sx;
+	s[1][1] = sy;
+	return s;
 }
 
 // The input argument is in degrees counterclockwise
 Matrix3x3 rotate(float deg) {
 	// Part 3: Fill this in.
-
-
-	return Matrix3x3();
+	auto r = Matrix3x3::identity();
+	r[0][0] = cos(radians(deg));
+	r[1][0] = -sin(radians(deg));
+	r[0][1] = sin(radians(deg));
+	r[1][1] = cos(radians(deg));
+	return r;
 }
 
 }
